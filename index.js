916eda9b140a2120b7e8c46ae1f83b50d1ea8c74
nan-mu <mu.nan@88.com>
2022-10-01 20:15:43 +0800

    信了网上的规划 看来要学adb和pm了/(ㄒoㄒ)/~~

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import { exec } from "./shell.js"
-import fs from "fs-extra"
+import { outputFile } from "fs-extra"
 
 (async () => {
     try {
-        await exec("chcp 65001");
-        console.log(await exec("echo 在？ ?"));
+        await exec("chcp 65001");//重生之我在node用system();
+        const packageNames = String.prototype.split.call(await exec("adb shell pm list packages"), "package:");
+        packageNames.shift();
+        //await outputFile("temp.txt", packageNames.toString());
+        for(let item in packageNames){
+            console.log(await exec(`adb shell pm path ${packageNames[item]}`));
+        }
     } catch (error) {
         console.error(error);
     }
